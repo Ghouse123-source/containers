7052dca32e6e773c27d8a4c73691ed0d3fa27876
ghousebashashaik <ghousebashashaik214@gmail.com>
2021-02-07 16:58:02 +0530

    ghouse adding file2.py in handle-on-exper

---
+++ file2.py
@@ -0,0 +1,22 @@
+print("ghouse basha shaik")
+print("o----")
+print("llll")
+print("*" * 10)
+price = 10
+print(price)
+
+
+varaibles
+---------
+name = 'Ghouse'
+age = 20
+rating = 4.9
+price = 30
+is_publisehed = True
+
+name = input('what is your name? ')
+print('hi ' + name)
+
+name = input('what is your name? ')
+colour = input('what is your fav colour? ')
+print('likes ' + name + colour)
